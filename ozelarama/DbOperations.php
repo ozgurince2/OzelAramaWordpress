d42ee51dd6c176d70154f2c3f9ce2d51f0cca266
Özgür <oince026@gmail.com>
2015-04-22 15:38:36 +0300

    some codes deleted

--- ozelarama/DbOperations.php
+++ ozelarama/DbOperations.php
@@ -12,35 +12,15 @@ class DbOperations{
 		else
 			$this->connection = $conn;
 	}
-/*
+
 	function search($key){
 
 		mysql_query("SET NAMES UTF8"); // Türkçe karakter sorununu ortadan kaldırır.
 		$result = mysqli_query($this->connection, 
-			"SELECT * FROM blog_posts JOIN categories on blog_posts.postID=categories.categoryID WHERE blog_posts.postContent like '%$key%';");
-		return $result;
-
-	}
-*/
-	function search($key){
-
-		mysql_query("SET NAMES UTF8"); // Türkçe karakter sorununu ortadan kaldırır.
-	/*	$result = mysqli_query($this->connection, 
-			"SELECT * FROM wp_posts JOIN categories on blog_posts.postID=categories.categoryID WHERE blog_posts.postContent like '%$key%';");
-	*/	$result = mysqli_query($this->connection, 
 			"SELECT * FROM wp_posts WHERE wp_posts.post_content like '%$key%';");
-	return $result;
-
-	}
-/*
-	function commentSearch($key){
-
-		$result = mysqli_query($this->connection, 
-			"SELECT * FROM comments JOIN blog_posts on blog_posts.postID=comments.postID WHERE comments.comment like '%$key%';");
-		mysql_unbuffered_query('SET NAMES utf8');
 		return $result;
 	}
-*/
+
 	function commentSearch($key){
 
 		$result = mysqli_query($this->connection, 
